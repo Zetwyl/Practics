0446fdb02f8cfb6a3922e6d8b6f429e9a501801d
Zetwyl <r-sherbinin70@mail.ru>
2026-04-02 21:39:16 +0300

    Add Pract_11

---
+++ Pract_11/Pract_11/ADO/AppData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract_11.ADO
+{
+    public static class AppData
+    {
+        public static db1Entities1 db1 = new db1Entities1();
+    }
+}
